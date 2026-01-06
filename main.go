d9609f2f43b4f3756737aec1ddc63c09e706ba41
Maharavan <90346511+Maharavan@users.noreply.github.com>
2026-01-06 09:31:32 +0530

    add: base62 encoder to avoid collisions

--- main.go
+++ main.go
@@ -14,24 +14,22 @@ import (
 var defaultScheme string = "http"
 var defaultHost string = "localhost:8000"
 
+const base62Digits = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 func random_number_within_range(min, max int) int {
 	return min + rand.IntN(max-min+1)
 }
 
-func generateRandomString() string {
-	var alphabet = [35]string{"a", "b", "c", "d", "e", "f", "g", "h", "i",
-		"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-		"0", "1", "2", "3", "4", "6", "7", "8", "9"}
-	random_string := ""
-	min := 0
-	max := len(alphabet) - 1
-	for i := 0; i < 7; i++ {
-		random_index := random_number_within_range(min, max)
-		random_string += alphabet[random_index]
+func base62Encoder(min, max int) string {
+	number := random_number_within_range(min, max)
+	base62 := ""
+
+	for number > 0 {
+		remainder := number % 62
+		base62 += string(base62Digits[remainder])
+		number /= 62
 	}
-
-	return random_string
-
+	return base62
 }
 
 func getHostNameandScheme() (string, string) {
@@ -72,13 +70,12 @@ func main() {
 	fmt.Println("Raw query: ", u.RawQuery)
 	fmt.Println("Fragment: ", u.Fragment)
 
-	get_random_string := generateRandomString()
+	get_random_string := base62Encoder(10000, 80000)
 	host, scheme := getHostNameandScheme()
-	fmt.Println(get_random_string)
-
+	fmt.Println(host, scheme, get_random_string)
 	construct_new_url := url.URL{
-		Scheme: host,
-		Host:   scheme,
+		Scheme: scheme,
+		Host:   host,
 		Path:   get_random_string,
 	}
 	http.HandleFunc("/"+get_random_string, func(w http.ResponseWriter, r *http.Request) {
