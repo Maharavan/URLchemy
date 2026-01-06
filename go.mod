92367e34f117aff3565d1e4c29e2569170b404b6
Maharavan <90346511+Maharavan@users.noreply.github.com>
2026-01-06 18:38:01 +0530

    url shortner via handlefunc and redirected the longurl with short generated url

--- go.mod
+++ go.mod
@@ -3,5 +3,3 @@ module urlshortner
 go 1.24.4
 
 require github.com/joho/godotenv v1.5.1
-
-require golang.org/x/text v0.32.0 // indirect
--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
+	"sync"
 
 	"github.com/joho/godotenv"
 )
 
 const base62Digits = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+type Payload struct {
+	URL string `json:"url"`
+}
+
+var (
+	inmemory = make(map[string]string)
+	mu       sync.RWMutex
+)
 
 func generateRandomBytes() []byte {
 
@@ -35,9 +48,7 @@ func base62Encoder() string {
 
 func getHostNameandScheme() (string, string) {
 	var defaultScheme string = "http"
-	var defaultHost string = "localhost:8000"
-
-	godotenv.Load()
+	var defaultHost string = "127.0.0.1:8000"
 
 	if hostname := os.Getenv("APP_HOST_NAME"); hostname != "" {
 		defaultHost = hostname
@@ -49,20 +60,33 @@ func getHostNameandScheme() (string, string) {
 	return defaultHost, defaultScheme
 }
 
-func main() {
+func retrievelongurl(w http.ResponseWriter, r *http.Request) {
 
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid Request Method", http.StatusMethodNotAllowed)
+		return
+	}
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("URL shortner failed due to ", r)
-			os.Exit(1)
+			panic(r)
 		}
 	}()
-	fmt.Println("URL Shortener Service")
 
-	u, err := url.Parse("https://www.example.com/path/to/resource?param1=value1&param2=value2#fragment")
+	var p Payload
+
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	dummy_url := p.URL
+	u, err := url.Parse(dummy_url)
 	if err != nil {
 		panic(err)
 	}
+
+	var get_random_string string
 
 	fmt.Printf("Type  %T", u)
 	fmt.Println("Current URL: ", u)
@@ -71,21 +95,70 @@ func main() {
 	fmt.Println("Path: ", u.Path)
 	fmt.Println("Raw query: ", u.RawQuery)
 	fmt.Println("Fragment: ", u.Fragment)
+	hostname, scheme := getHostNameandScheme()
+	for {
+		get_random_string = base62Encoder()
+		if _, ok := inmemory[get_random_string]; !ok {
+			mu.Lock()
+			inmemory[get_random_string] = u.String()
+			mu.Unlock()
+			break
+		}
 
-	get_random_string := base62Encoder()
-	host, scheme := getHostNameandScheme()
+	}
+
 	construct_new_url := url.URL{
 		Scheme: scheme,
-		Host:   host,
+		Host:   hostname,
 		Path:   get_random_string,
 	}
-	http.HandleFunc("/"+get_random_string, func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, u.String(), http.StatusTemporaryRedirect)
-	})
-	fmt.Println("Redirected URL:", construct_new_url.String())
+	url_data := Payload{
+		URL: construct_new_url.String(),
+	}
 
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		panic(err)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(url_data)
+}
+
+func rerouter(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid Request Method", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Println(r.URL)
+	random := strings.TrimPrefix(r.URL.Path, "/")
+	if random == "" {
+		http.Error(w, fmt.Sprintf("%v", inmemory), http.StatusNotFound)
+		return
+	}
+
+	mu.RLock()
+
+	longurl, ok := inmemory[random]
+	mu.RUnlock()
+
+	if !ok {
+		http.Error(w, "Invalid data", http.StatusNotFound)
+		return
+	}
+
+	http.Redirect(w, r, longurl, http.StatusMovedPermanently)
+
+}
+
+func main() {
+	godotenv.Load()
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /longurl", retrievelongurl)
+	mux.HandleFunc("GET /", rerouter)
+	log.Print("URL Shortner Service.....")
+	log.Print("Connecting to Port")
+	port := ":8000"
+
+	if err := http.ListenAndServe(port, mux); err != nil {
+		log.Fatal(err)
 	}
 
 }
